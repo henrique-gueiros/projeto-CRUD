4750c9b7f9191e304b48b9b3c7a844a154b6844c
Samuel Lucas <sam.abreugouveia@gmail.com>
2024-05-09 11:19:50 -0300

    iniciando a class

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import os 
-os.system("cls")
+import csv
+ #manos, a gente vai usar esse csv depois e vai colocar pra limpar o terminal depois tambem pra poder organizar melhor os "os" que a gente importar aqui
+ #esse def dos componentes a gente vai usar pra armazenar os dados de país nome da receita e etc
 
-
+class receita:
+    def componentes(self, nome, pais, ingredientes, preparo):
+        self.nome = nome 
+        self.pais = pais 
+        self.ingredientes = ingredientes 
+        self.preparo = preparo
