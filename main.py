9e9a9d55def216a9149143d3ecdf208df4b2b4fa
henrique-gueiros <riquewg@gmail.com>
2024-05-09 22:17:38 -0300

    teste

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ os.system('cls')
 import csv
  #manos, a gente vai usar esse csv depois e vai colocar pra limpar o terminal depois tambem pra poder organizar melhor os "os" que a gente importar aqui
  #esse def dos componentes a gente vai usar pra armazenar os dados de país nome da receita e etc
-
+ #testando
 class receita:
     def __init__(self, nome, pais, ingredientes, preparo, favorito): 
         pass
