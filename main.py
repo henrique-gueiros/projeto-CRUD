3932aa1bc7d4cd735eaec69d457d93a5e6b11b39
henrique-gueiros <riquewg@gmail.com>
2024-05-11 12:22:53 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import csv
  #manos, a gente vai usar esse csv depois e vai colocar pra limpar o terminal depois tambem pra poder organizar melhor os "os" que a gente importar aqui
  #esse def dos componentes a gente vai usar pra armazenar os dados de país nome da receita e etc
 
-class receita:
+class Receita:
     def __init__(self, nome, pais, ingredientes, preparo, favorito): 
         pass
         self.nome = nome
@@ -12,6 +12,31 @@ class receita:
         self.ingredientes = ingredientes
         self.preapro = preparo
         self.favorito = favorito
+
+    def banco_dados():
+        with open('receitas.csv', 'r', encoding='utf8') as filecsv:
+            dados=[]
+            for i in filecsv:
+                nome, pais, ingredientes, preparo, favorito = i
+                ingredientes = ingredientes.split(', ') #separei os ingredientes por vírgula pra ficar mais fácil
+                receita = Receita(nome, pais, ingredientes, preparo, favorito)
+                dados.append(receita)
+        return dados
+
+    #def p/ adicionar receita no banco de dados == csv
+    def add():
+        with open('receitas.csv','w', encoding='utf8') as filecsv:
+
+            nome = input('Nome de receita:')
+            pais = input('país de origem da receita: ')
+            ingredientes = input("Ingredientes: ").split(", ")
+            modo_preparo = input("Modo de preparo: ")
+
+            filecsv.writelines(nome, pais, ingredientes, modo_preparo)
+
+        
+        
+    
 
 
 
